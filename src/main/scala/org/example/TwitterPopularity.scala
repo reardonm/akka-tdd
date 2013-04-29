75962ce81c6e65a03118c9a52bbe90f80a118b70
Mike Reardon <michael.reardon@mtnsat.com>
2013-04-28 23:38:47 -0700

    Single test

--- src/main/scala/org/example/TwitterPopularity.scala
+++ src/main/scala/org/example/TwitterPopularity.scala
@@ -1,6 +1,38 @@
 package org.example
 
+import akka.actor.{ActorLogging, Actor}
+
 
 object TwitterPopularity extends App {
   println("Hello World")
+}
+
+object TwitterFeedProtcol {
+  case class Start(hashTag: String)
+  case object Stop
+  case object Busy_?
+  case object No
+  case class Yes(hashTag: String)
+}
+
+class TwitterFeedConsumer extends Actor with ActorLogging {
+  import TwitterFeedProtcol._
+
+  def stopped: Receive = {
+    case Start(hashTag) =>
+      println("Start")
+      context.become(started(hashTag))
+    case Busy_? =>
+      sender ! No
+  }
+
+  def started(hashTag: String): Receive = {
+    case Stop =>
+      println("Stop")
+      context.become(stopped)
+    case Busy_? =>
+      sender ! Yes(hashTag)
+  }
+
+  def receive = stopped
 }
--- src/test/scala/org/example/TwitterFeedConsumerSpec.scala
+++ src/test/scala/org/example/TwitterFeedConsumerSpec.scala
@@ -2,8 +2,9 @@ package org.example
 
 import org.scalatest.{WordSpec, BeforeAndAfterAll}
 import org.scalatest.matchers.ShouldMatchers
-import akka.actor.ActorSystem
-import akka.testkit.{ImplicitSender, TestKit}
+import akka.actor.{Props, ActorSystem}
+import akka.testkit.{TestKit, TestActorRef, ImplicitSender}
+import akka.dispatch.Await
 
 class TwitterFeedConsumerSpec extends TestKit(ActorSystem("TwitterFeedSpec"))
   with ImplicitSender
@@ -11,19 +12,27 @@ class TwitterFeedConsumerSpec extends TestKit(ActorSystem("TwitterFeedSpec"))
   with ShouldMatchers
   with BeforeAndAfterAll {
 
+  import TwitterFeedProtcol._
+
+  override def afterAll() = system.shutdown()
+
   "TwitterFeedConsumer" when {
 
     "initialized" should {
-      "be inactive" in pending
+      "be inactive" in {
+        val actorRef = TestActorRef[TwitterFeedConsumer]
+        actorRef ! Busy_?
+        expectMsg(No)
+      }
     }
 
     "stopped" should {
       "do nothing for stop message" in pending
-      "change to reading state for hashtag message" in pending
-     }
-
+      "change to reading state for hashtag message" in  pending
     "reading" should {
       "send user for each tweet to the UserRegistry actor" in pending
+      }
+
       "change to stopped state for stop message" in pending
     }
   }
